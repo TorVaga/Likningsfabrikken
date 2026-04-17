60b0db4f61499920b5b5233617eb4d81785793b2
TorVaga <torbjornsa@gmail.com>
2026-04-17 11:38:37 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
+console.log("JEG ER LIVE")
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 )
